9efca2d6dafb00c05d2a9978c2d74468c7667ac0
Subrat29 <ankubanku29062003@gmail.com>
2023-12-09 18:54:40 +0530

    day69

--- Trees/Predecessor and Successor/app1.cpp
+++ Trees/Predecessor and Successor/app1.cpp
@@ -17,6 +17,56 @@ struct Node
     }
 };
 
+void inorder(Node *root, vector<Node *> &arr)
+{
+    if (root == nullptr)
+        return;
+    inorder(root->left, arr);
+
+    arr.push_back(root);
+
+    inorder(root->right, arr);
+}
+void findPreSuc(Node *root, Node *&pre, Node *&suc, int key)
+{
+    // Your code goes here
+    vector<Node *> arr;
+    inorder(root, arr);
+    suc = pre = nullptr;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i]->key == key)
+        {
+            if (i == 0)
+            {
+                pre = nullptr;
+            }
+            else
+            {
+                pre = arr[i - 1];
+            }
+            if (i == arr.size() - 1)
+            {
+                suc = nullptr;
+            }
+            else
+            {
+                suc = arr[i + 1];
+            }
+            break;
+        }
+        else if (arr[i]->key < key)
+        {
+            pre = arr[i];
+        }
+        else
+        {
+            suc = arr[i];
+            break;
+        }
+    }
+}
+
 int main()
 {
     return 0;
