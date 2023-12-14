126cb34b3288428900ed38de551fc77c7a21b56f
Subrat29 <ankubanku29062003@gmail.com>
2023-12-14 22:55:32 +0530

    day74

---
+++ Trees/102. Binary Tree Level Order Traversal/app1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void solve(TreeNode *root, vector<vector<int>> &ans)
+{
+    vector<int> v;
+    queue<TreeNode *> q;
+    q.push(root);
+    q.push(nullptr);
+
+    while (!q.empty())
+    {
+        TreeNode *temp = q.front();
+        q.pop();
+
+        if (temp == nullptr)
+        {
+            ans.push_back(v);
+            v.clear();
+            if (!q.empty())
+                q.push(nullptr);
+        }
+        else
+        {
+            v.push_back(temp->val);
+
+            if (temp->left)
+                q.push(temp->left);
+
+            if (temp->right)
+                q.push(temp->right);
+        }
+    }
+}
+
+vector<vector<int>> levelOrder(TreeNode *root)
+{
+    vector<vector<int>> ans;
+    if (root)
+        solve(root, ans);
+    return ans;
+}
+
+int main()
+{
+    return 0;
+}
---
+++ Trees/437. Path Sum III/app1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int ans = 0;
+int pathSum(TreeNode *root, int sum)
+{
+    if (root)
+    {
+        dfs(root, sum);
+        pathSum(root->left, sum);
+        pathSum(root->right, sum);
+    }
+    return ans;
+}
+void dfs(TreeNode *root, int sum)
+{
+    if (!root)
+        return;
+    if (root->val == sum)
+        ans++;
+    dfs(root->left, sum - root->val);
+    dfs(root->right, sum - root->val);
+}
+
+int main()
+{
+    return 0;
+}
