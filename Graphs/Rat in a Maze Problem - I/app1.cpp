761943e42df64dabf0303e979a0aea01864322df
Subrat29 <ankubanku29062003@gmail.com>
2023-12-28 14:01:16 +0530

    day87

---
+++ Graphs/Rat in a Maze Problem - I/app1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool isSafe(int newx, int newy, vector<vector<bool>> &vis, vector<vector<int>> &arr, int n)
+{
+    if ((newx >= 0 && newx < n) && (newy >= 0 && newy < n) && vis[newx][newy] != 1 && arr[newx][newy] == 1)
+    {
+        return true;
+    }
+    else
+    {
+
+        return false;
+    }
+}
+
+void solve(int x, int y, vector<vector<int>> &arr, int n, vector<string> &ans, vector<vector<bool>> &vis, string path)
+{
+    // base case: destination reached
+    if (x == n - 1 && y == n - 1)
+    {
+        ans.push_back(path);
+        return;
+    }
+
+    // 4 movement: D, L, R, U
+
+    vis[x][y] = 1;
+
+    // Down
+    if (isSafe(x + 1, y, vis, arr, n))
+    {
+        solve(x + 1, y, arr, n, ans, vis, path + 'D');
+    }
+
+    // Left
+    if (isSafe(x, y - 1, vis, arr, n))
+    {
+        solve(x, y - 1, arr, n, ans, vis, path + 'L');
+    }
+
+    // Right
+    if (isSafe(x, y + 1, vis, arr, n))
+    {
+        solve(x, y + 1, arr, n, ans, vis, path + 'R');
+    }
+
+    // Up
+    if (isSafe(x - 1, y, vis, arr, n))
+    {
+        solve(x - 1, y, arr, n, ans, vis, path + 'U');
+    }
+
+    vis[x][y] = 0;
+}
+
+vector<string> searchMaze(vector<vector<int>> &arr, int n) // Tc: 4^n*n, Sc: m*n
+{
+    vector<string> ans;
+    vector<vector<bool>> visited(n, vector<bool>(n, 0));
+    string path = "";
+
+    // edge case
+    if (arr[0][0] == 0)
+        return ans;
+
+    solve(0, 0, arr, n, ans, visited, path);
+    return ans;
+}
+
+int main()
+{
+    return 0;
+}
